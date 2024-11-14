3fb6608d9846ec74997a9ea7c8b6dbc7263190c6
djdj4275 <djdj4275@naver.com>
2024-11-14 13:41:24 +0900

    test 수정중

---
+++ back/src/test/java/matnam_zang/demo/controller/UserControllerTest.java
@@ -0,0 +1,119 @@
+package matnam_zang.demo.controller;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import jakarta.transaction.Transactional;
+import matnam_zang.demo.repository.CategoryRepository;
+import matnam_zang.demo.repository.FavoriteRepository;
+import matnam_zang.demo.repository.ImageRepository;
+import matnam_zang.demo.repository.IngredientRepository;
+import matnam_zang.demo.repository.InstructionRepository;
+import matnam_zang.demo.repository.RecipeRepository;
+import matnam_zang.demo.repository.ReviewRepository;
+import matnam_zang.demo.repository.UserRepository;
+import matnam_zang.demo.security.TokenProvider;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+// 컨트롤러단에서의 통합테스트 (컨트롤러-서비스-레파지토리-db 까지의 api 전체로직을 검증하여 리턴값 검증)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)  // Spring Boot 애플리케이션의 컨텍스트 로드
+@AutoConfigureMockMvc  // MockMvc 자동 설정
+@TestMethodOrder(OrderAnnotation.class)  // 테스트 순서 지정
+@Transactional
+public class UserControllerTest {
+    
+    @Mock
+    private RecipeRepository recipeRepository;
+
+    
+    @Mock
+    private ImageRepository imageRepository;
+
+    
+    @Mock
+    private IngredientRepository ingredientRepository;
+
+    
+    @Mock
+    private InstructionRepository instructionRepository;
+
+    
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    
+    @Mock
+    private UserRepository userRepository;
+
+    
+    @Mock
+    private TokenProvider tokenProvider;
+
+    
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    
+    @Mock
+    private ReviewRepository reviewRepository;
+
+    
+    @Mock
+    private FavoriteRepository favoriteRepository;
+
+    @Autowired
+    private MockMvc mockMvc;  // MockMvc를 사용해 HTTP 요청을 시뮬레이션
+
+    private String jwtToken;  // JWT 토큰을 저장할 변수
+
+    // 회원가입
+    @Test
+    @Order(1)
+    void registerUser() throws Exception {
+        String userJson = "{\"username\":\"testuser\", \"password\":\"testpassword\", \"email\":\"test@example.com\"}";
+    
+        // 회원가입 요청을 보내기
+        mockMvc.perform(post("/api/auth/register")
+        .contentType("application/json")
+        .content(userJson))
+        .andExpect(status().isOk());  // 200 상태 코드를 기대
+    }
+
+    // 로그인 (트랜잭션은 각 메소드마다 개별적으로 종속되기때문에 롤백 회원가입시의 데이터를 활용할수없어 회원가입 로직 자체를 로그인에 포함시켜 진행)
+    @Test
+    @Order(2)
+    void loginUser() throws Exception {
+
+        String userJson = "{\"username\":\"testuser\", \"password\":\"testpassword\", \"email\":\"test@example.com\"}";
+    
+        // 회원가입 요청을 보내기
+        mockMvc.perform(post("/api/auth/register")
+        .contentType("application/json")
+        .content(userJson));
+
+        String loginJson = "{\"username\":\"testuser\", \"password\":\"testpassword\"}";
+    
+        // 로그인 요청을 보내기
+        this.jwtToken = mockMvc.perform(post("/api/auth/login")
+                .contentType("application/json")
+                .content(loginJson))
+                .andExpect(status().isOk())  // 200 상태 코드
+                .andExpect(jsonPath("$.token").exists())  // token 존재 확인
+                .andExpect(jsonPath("$.token").isString())  // token이 문자열인지 확인
+                .andReturn()
+                .getResponse()
+                .getContentAsString()
+                .split("\"token\":\"")[1].split("\"")[0];  // 토큰만 추출해서 저장
+    }
+}
--- back/src/test/java/matnam_zang/demo/service/UserServiceTest.java
+++ back/src/test/java/matnam_zang/demo/service/UserServiceTest.java
@@ -1,146 +1,78 @@
 package matnam_zang.demo.service;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.server.ResponseStatusException;
-
-import jakarta.transaction.Transactional;
-import matnam_zang.demo.dto.BoardRecipeDto;
-import matnam_zang.demo.dto.BoardRecipesDto;
-import matnam_zang.demo.dto.ImageDto;
-import matnam_zang.demo.dto.ReviewDto;
-import matnam_zang.demo.dto.UserRecipeDto;
-import matnam_zang.demo.entity.Category;
-import matnam_zang.demo.entity.Favorite;
-import matnam_zang.demo.entity.Image;
-import matnam_zang.demo.entity.Ingredient;
-import matnam_zang.demo.entity.Instruction;
-import matnam_zang.demo.entity.Recipe;
-import matnam_zang.demo.entity.Review;
 import matnam_zang.demo.entity.User;
-import matnam_zang.demo.repository.CategoryRepository;
-import matnam_zang.demo.repository.FavoriteRepository;
-import matnam_zang.demo.repository.ImageRepository;
-import matnam_zang.demo.repository.IngredientRepository;
-import matnam_zang.demo.repository.InstructionRepository;
-import matnam_zang.demo.repository.RecipeRepository;
-import matnam_zang.demo.repository.ReviewRepository;
 import matnam_zang.demo.repository.UserRepository;
 import matnam_zang.demo.security.TokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)  // Spring Boot 애플리케이션의 컨텍스트 로드
-@AutoConfigureMockMvc  // MockMvc 자동 설정
-@TestMethodOrder(OrderAnnotation.class)  // 테스트 순서 지정
-@Transactional
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// 서비스단의 단위테스트 (서비스단에서의 메소드만 확인하기때문에 전체적 로직과는 연관없이 서비스만 의존)
 public class UserServiceTest {
-    
-    @Mock
-    private RecipeRepository recipeRepository;
 
-    
-    @Mock
-    private ImageRepository imageRepository;
-
-    
-    @Mock
-    private IngredientRepository ingredientRepository;
-
-    
-    @Mock
-    private InstructionRepository instructionRepository;
-
-    
-    @Mock
-    private CategoryRepository categoryRepository;
-
-    
     @Mock
     private UserRepository userRepository;
 
-    
-    @Mock
-    private TokenProvider tokenProvider;
-
-    
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    
     @Mock
-    private ReviewRepository reviewRepository;
+    private TokenProvider tokenProvider;
 
-    
-    @Mock
-    private FavoriteRepository favoriteRepository;
+    @InjectMocks
+    private UserService userService;
 
-    @Autowired
-    private MockMvc mockMvc;  // MockMvc를 사용해 HTTP 요청을 시뮬레이션
-
-    private String jwtToken;  // JWT 토큰을 저장할 변수
-
-    // 회원가입
-    @Test
-    @Order(1)
-    void registerUser() throws Exception {
-        String userJson = "{\"username\":\"testuser\", \"password\":\"testpassword\", \"email\":\"test@example.com\"}";
-    
-        // 회원가입 요청을 보내기
-        mockMvc.perform(post("/api/auth/register")
-        .contentType("application/json")
-        .content(userJson))
-        .andExpect(status().isOk());  // 200 상태 코드를 기대
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
-    // 로그인
     @Test
-    @Order(2)
-    void loginUser() throws Exception {
+    void registerUser() {
+        // given
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("encodedPassword");
+        user.setEmail("test@example.com");
 
-        String userJson = "{\"username\":\"testuser\", \"password\":\"testpassword\", \"email\":\"test@example.com\"}";
-    
-        // 회원가입 요청을 보내기
-        mockMvc.perform(post("/api/auth/register")
-        .contentType("application/json")
-        .content(userJson));
+        when(passwordEncoder.encode("testpassword")).thenReturn("encodedPassword");
+        when(userRepository.save(any(User.class))).thenReturn(user);
 
-        String loginJson = "{\"username\":\"testuser\", \"password\":\"testpassword\"}";
-    
-        // 로그인 요청을 보내기
-        this.jwtToken = mockMvc.perform(post("/api/auth/login")
-                .contentType("application/json")
-                .content(loginJson))
-                .andExpect(status().isOk())  // 200 상태 코드
-                .andExpect(jsonPath("$.token").exists())  // token 존재 확인
-                .andExpect(jsonPath("$.token").isString())  // token이 문자열인지 확인
-                .andReturn()
-                .getResponse()
-                .getContentAsString()
-                .split("\"token\":\"")[1].split("\"")[0];  // 토큰만 추출해서 저장
+        // when
+        assertDoesNotThrow(() -> userService.registerUser(user));  // 메서드 호출 시 예외가 발생하지 않는지 확인
+
+        // then
+        verify(userRepository, times(1)).save(any(User.class));  // userRepository의 save 메서드가 한 번 호출됐는지 확인
+    }
+
+
+    @Test
+    void loginUser() {
+        // given
+        User user = new User();
+        user.setUsername("testuser");
+        user.setPassword("encodedPassword");
+
+        when(userRepository.findByUsername("testuser")).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("testpassword", "encodedPassword")).thenReturn(true);
+        when(tokenProvider.createToken("testuser")).thenReturn("dummyJwtToken");
+
+        // when
+        String token = userService.loginUser("testuser", "testpassword");
+
+        // then
+        assertNotNull(token);
+        assertEquals("dummyJwtToken", token);
+        verify(tokenProvider, times(1)).createToken("testuser");
     }
 }
