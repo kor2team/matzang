fba1431e5aacf193d9fe4b61e31ef037d79d7a2c
ckstnr99 <djdj4275@naver.com>
2024-11-15 21:37:02 +0900

    update: youtube 테스트 코드 작성

--- back/src/main/java/matnam_zang/demo/controller/YoutubeController.java
+++ back/src/main/java/matnam_zang/demo/controller/YoutubeController.java
@@ -2,6 +2,7 @@ package matnam_zang.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,10 @@ import matnam_zang.demo.service.YoutubeService;
 @RestController
 @RequestMapping("/api")
 public class YoutubeController {
+
+    @Value("${api.youtubeKey}")
+    private String youtubeKey;
+
     private final YoutubeService youtubeService;
 
     public YoutubeController(YoutubeService youtubeService) {
@@ -23,7 +28,7 @@ public class YoutubeController {
     // 유튜브 이름조회 엔드포인트
     @GetMapping("/youtube/{searchName}")
     public List<YouTubeDto> getYoutubeBySearchNameMethod(@PathVariable("searchName") String searchName) {
-        return youtubeService.getYoutubeBySearchName(searchName);
+        return youtubeService.getYoutubeBySearchName(searchName, youtubeKey);
     }
     
 }
